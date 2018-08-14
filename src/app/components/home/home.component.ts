9a8febe84af7aa63bc24c01e800fb95398878d17
Kevin <kevin@reponic.org>
2018-08-14 09:57:57 -0400

    rer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from '../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService : AuthService,
+              private router : Router) { }
 
   ngOnInit() {
+    if(!this.authService.isAuthenticated()){
+      this.router.navigate(['login']);
+    }
   }
 
 }
