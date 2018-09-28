ac293eae1be089284e9f133d79a7543b94dfe3dd
kevinreponic <41744588+kevinreponic@users.noreply.github.com>
2018-09-28 18:32:08 -0400

    Cloudinary integration (image uploading)

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -141,4 +141,43 @@ export class DatabaseService {
     return this.http.post('http://localhost:8080/api/update-user', user, {headers : headers}).pipe(map(res=>res.json()));
   }
 
+  loadImg(file) {
+    return new Promise((resolve, reject) => {
+      let tipo;
+      if (file) {
+        switch (file.type) {
+          case 'image/gif':
+            tipo = 'data:image/gif;base64,';
+            break;
+          case 'image/jpeg':
+            tipo = 'data:image/jpeg;base64,';
+            break;
+          case 'image/png':
+            tipo = 'data:image/png;base64,';
+            break;
+          case 'image/svg+xml':
+            tipo = 'data:image/svg+xml;base64,';
+            break;
+          default:
+            tipo = null;
+            return;
+        }
+        const reader = new FileReader();
+        if(tipo) {
+          reader.onload = readEvt => {
+            const binaryString = (readEvt.target as any).result;
+            const base64 = btoa(binaryString);
+            const resultado = tipo + base64;
+            resolve(resultado);
+          }
+          reader.readAsBinaryString(file);
+        } else {
+          reject('Invalid image format')
+        }
+      } else {
+        reject('Invalid image format');
+      }
+    });
+  }
+
 }
